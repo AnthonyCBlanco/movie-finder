70683ac3c0820de1125f3300e1ae8f8ca2374fbd
Anthony Blanco <anthony200527@outlook.com>
2023-12-06 19:39:07 -0800

    Add Comments describing what each function is doing

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,8 @@
 
 
 // Function to convert fetch Data To Obj
+// Obj = The variable you would like to store the Fetched Data in
+// Url = The Url to fetch from
 async function fetchData(obj, url) {
    try {
      const response = await fetch(url);
@@ -21,7 +23,7 @@ async function fetchData(obj, url) {
      throw error;
    }
  }
-
+ // Builds Carousel Cards
  async function trendingBuildCarousel(){
    let trendingMoviesUrl = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=d49378c8d91fbf3feb27659eb9dad49e'
    let trendingMovies; 
@@ -38,10 +40,9 @@ const searchBtnEl = document.querySelector('#search-btn')
 
 
 //searches for TMDB with input from search box
-
 async function searchForMovie(){ 
   var searchResultsContainer = document.querySelector('#search-results')
-  searchResultsContainer.innerHTML = ""
+  searchResultsContainer.innerHTML = "" 
   var currentUrl = new URL(window.location.href);
   var searchTerm = document.querySelector('.search-input').value
   currentUrl.searchParams.set('searchTerm', searchTerm)
@@ -51,22 +52,42 @@ async function searchForMovie(){
   let searchUrl = 'https://api.themoviedb.org/3/search/movie?query=' + searchTerm + '&api_key=d49378c8d91fbf3feb27659eb9dad49e' 
   let searchData;
   searchData = await fetchData(searchData, searchUrl)
-
-  for(var i=0; i < searchData.results.length; i++){
-    var li = document.createElement('li')
-    var img = document.createElement('img')
-    li.textContent = searchData.results[i].title
-    li.setAttribute('class', 'column is-one-quarter is')
-    img.src = 'https://image.tmdb.org/t/p/w500/' + searchData.results[i].poster_path
-    li.append(img)
-    searchResultsContainer.append(li)
-  }
+  // Error Messege If No Results Found
+    if(searchData.results.length == 0){ 
+      searchResultsContainer.textContent = ('Sorry, We Couldnt Find Anything Under That Name Please Try Again')
+      return
+    }
+  // Builds Search Results Cards
+    for(var i=0; i < searchData.results.length; i++){ 
+      var li = document.createElement('li')
+      var img = document.createElement('img')
+      li.textContent = searchData.results[i].title
+      li.setAttribute('class', 'column is-one-quarter is')
+      img.src = 'https://image.tmdb.org/t/p/w500/' + searchData.results[i].poster_path
+      li.append(img)
+      searchResultsContainer.append(li)
+    }
 
   console.log(searchData)
 }
 
+// Fetchs details of Selected Movie And Builds The Details Page
+function fetchDetails(){
+
+}
+
+
+
+
+
+
+
+
+
+
 
 var currentPage = window.location.pathname
+// Only runs this code if on Main Page
 if(currentPage.endsWith('index.html') || currentPage.endsWith('movie-finder/')){
   trendingBuildCarousel()
   var homesearchBtnEl = document.querySelector('#search-btn-to-search')
@@ -74,6 +95,7 @@ if(currentPage.endsWith('index.html') || currentPage.endsWith('movie-finder/')){
     document.location.href = './search.html'
   })
 }
+// Only runs this code if on search page
 if(window.location.href.includes('search.html')){
     searchBtnEl.addEventListener('click', function(event){
       event.preventDefault()
